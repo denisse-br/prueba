4d4cf81b75a2b20b2f5c2dc541f92ad403e8a06f
denisse-br <denisse294@live.com>
2016-10-21 00:24:06 -0500

    usuario default *arreglar*

--- app/src/main/java/com/ingenieria/oscarol/prueba/Main2Activity.java
+++ app/src/main/java/com/ingenieria/oscarol/prueba/Main2Activity.java
@@ -2,11 +2,15 @@ package com.ingenieria.oscarol.prueba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
@@ -18,8 +22,11 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         iniciarVar();
 
-    }
 
+
+    }
+    String userDef="";
+    String[] userTemp;
     private void iniciarVar() {
         dpi= (EditText)findViewById(R.id.dpi);
         nombre=(EditText)findViewById(R.id.nombre);
@@ -29,10 +36,31 @@ public class Main2Activity extends AppCompatActivity {
         user=(EditText)findViewById(R.id.user);
         dept=(EditText)findViewById(R.id.dept);
         estado=(Switch) findViewById(R.id.switch1);
+
+        nombre.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if(b==true){
+                    if(nombre.getText().toString().isEmpty()){
+
+                    }else{
+                        userTemp=nombre.getText().toString().split(" ");
+                        userDef=userTemp[0];
+                        user.setText(userDef);
+                        Log.i("info",userDef);
+                    }
+
+
+                }
+            }
+        });
+
     }
 
 
     public void registrar(View view) {
+
+
         if(dpi.getText().length()==0||nombre.getText().length()==0||mail.getText().length()==0||pass.getText().length()==0||fecha.getText().length()==0||user.getText().length()==0||dept.getText().length()==0){
             Toast.makeText(getApplicationContext(),"Ningun campo puede quedar vacio",Toast.LENGTH_LONG).show();
         }else{
