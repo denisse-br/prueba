9fb0ed26328aed4eb59b42b168cdcae921999ee7
denisse-br <denisse294@live.com>
2016-10-21 05:47:41 -0500

    Conexion con servidor

--- app/src/main/java/com/ingenieria/oscarol/prueba/ObtenerDatos.java
+++ app/src/main/java/com/ingenieria/oscarol/prueba/ObtenerDatos.java
@@ -5,13 +5,18 @@ import android.util.Log;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  * Created by Denissebr on 20/10/2016.
  */
 public class ObtenerDatos {
-
+    private Socket client;
     public void obtenerDatosRegistro(long dpi, String nombre, String mail, String dept, String pass, String fecha, String user, boolean estado ){
-        JSONObject jsonRegistrar =  new JSONObject();
+        final JSONObject jsonRegistrar =  new JSONObject();
         try {
             jsonRegistrar.put("dpi",dpi);
             jsonRegistrar.put("nombre",nombre);
@@ -30,6 +35,32 @@ public class ObtenerDatos {
             e.printStackTrace();
         }
 
-        Log.i("JSON",":"+jsonRegistrar);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    client = new Socket("192.168.1.2",2222);
+
+                    PrintWriter printWriter = new PrintWriter(client.getOutputStream());
+                    printWriter.write("{Registro:[{"+String.valueOf(jsonRegistrar)+"}]}");
+                    printWriter.flush();
+                    printWriter.close();
+                    client.close();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+
+
+
+
+
+
+        //Log.i("JSON",":"+jsonRegistrar);
     }
 }
